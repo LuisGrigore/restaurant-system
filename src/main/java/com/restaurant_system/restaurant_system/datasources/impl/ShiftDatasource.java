50d0ba6a2fe328852a0dfb8a97a276b33ac6272a
LuisGrigore <luiscristiangrigore11@gmail.com>
2024-12-31 15:37:24 +0100

    shift controller

--- src/main/java/com/restaurant_system/restaurant_system/datasources/impl/ShiftDatasource.java
+++ src/main/java/com/restaurant_system/restaurant_system/datasources/impl/ShiftDatasource.java
@@ -2,7 +2,9 @@ package com.restaurant_system.restaurant_system.datasources.impl;
 
 import com.restaurant_system.restaurant_system.datasources.IShiftDatasource;
 import com.restaurant_system.restaurant_system.model.ShiftEntity;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ShiftDatasource implements IShiftDatasource {
     @Override
     public void sendShift(ShiftEntity shift) {
